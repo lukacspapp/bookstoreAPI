328ad9252070e1b7941e6423ab76cfc6be1bd28a
Lukacs J Papp <papplukacs@hotmail.com>
2024-01-21 00:55:26 +0000

    removed unnescesary coms

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Remove or modify this if you don't have a setup file
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1', // Adjust this if you have a specific way to resolve modules
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['/node_modules/'],
   verbose: true,
